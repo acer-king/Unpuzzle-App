a68949ecfefaf415d5ee1089b92179ad3173d047
Mahtab Alam <muscledagency@gmail.com>
2020-05-08 19:55:29 -0600

    token implemented

---
+++ dbschema.js
@@ -0,0 +1,40 @@
+let db = {
+  puzzlepieces: [
+    {
+      userHandle: 'user',
+      body: 'this is a PP',
+      createdAt: '2020-05-06T14:22:21.576Z',
+      ppType: 'Video',
+      ppURL: 'PP URL.com',
+      likeCount: 23,
+      commentCount: 10,
+      childPuzzlePieces: [
+        {
+          userHandle: 'user',
+          body: 'this is a PP',
+          createdAt: '2020-05-06T14:22:21.576Z',
+          ppType: 'Video',
+          ppURL: 'PP URL.com',
+          likeCount: 23,
+          commentCount: 10
+        }
+      ],
+      parentPuzzlePieces: [
+        {
+          userHandle: 'user',
+          body: 'this is a PP',
+          createdAt: '2020-05-06T14:22:21.576Z',
+          ppType: 'Video',
+          ppURL: 'PP URL.com',
+          likeCount: 23,
+          commentCount: 10
+        }
+      ],
+      tags: [
+        {
+          tagNames: ['coding', 'javascript', 'node', 'react']
+        }
+      ]
+    }
+  ]
+}
