fd9cd2989758b7a8be850f6fb5b564710689161f
Mahtab Alam <roadtocode822@gmail.com>
2020-05-18 14:30:30 -0400

    trial commit 2

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -19,7 +19,7 @@ exports.signup = (req, res) => {
 
   if(!valid) return res.status(400).json(errors);
 
-  let token, userId, uuuser;
+  let token, userId;
 
   db
     .doc(`/users/${newUser.handle}`)
