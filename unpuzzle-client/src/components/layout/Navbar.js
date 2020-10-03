29cd5a232c3d1e90c3b1a51705e4c6b2156ba40a
Mahtab Alam <roadtocode822@gmail.com>
2020-10-02 21:20:24 -0400

    Prettier fixed so home and footer look cleaner. Issue with lottie animation inside grid item. Won't flex end.

--- unpuzzle-client/src/components/layout/Navbar.js
+++ unpuzzle-client/src/components/layout/Navbar.js
@@ -1,66 +1,66 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import MyButton from '../../util/MyButton';
-import PostPuzzlepiece from '../puzzlepiece/PostPuzzlepiece';
-import Notifications from './Notifications';
-import withStyles from '@material-ui/core/styles/withStyles';
+import React, { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import MyButton from "../../util/MyButton";
+import PostPuzzlepiece from "../puzzlepiece/PostPuzzlepiece";
+import Notifications from "./Notifications";
+import withStyles from "@material-ui/core/styles/withStyles";
 // MUI stuff
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import {useTheme} from '@material-ui/core/styles';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 // Icons
-import HomeIcon from '@material-ui/icons/Home';
-import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from "@material-ui/icons/Home";
+import MenuIcon from "@material-ui/icons/Menu";
 // Logo
-import upLogo from '../../images/upLogo.svg';
+import upLogo from "../../images/upLogo.svg";
 
-const styles = theme => ({
+const styles = (theme) => ({
   ...theme.themeStyle,
   toolbarMargin: {
-      ...theme.mixins.toolbar,
-      marginBottom: "1.1em",
-      [theme.breakpoints.down("md")]: {
-        marginBottom: "1em"
-      },
-      [theme.breakpoints.down("xs")]: {
-        marginBottom: "1.25em"
-      }
+    ...theme.mixins.toolbar,
+    marginBottom: "1.1em",
+    [theme.breakpoints.down("md")]: {
+      marginBottom: "1em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: "1.25em",
+    },
   },
   logoContainer: {
     padding: 0,
     "&:hover": {
-      backgroundColor: "transparent"
-    }
+      backgroundColor: "transparent",
+    },
   },
   headerLogo: {
     height: "6em",
     [theme.breakpoints.down("md")]: {
-      height: "5em"
+      height: "5em",
     },
     [theme.breakpoints.down("xs")]: {
-      height: "4.5em"
-    }
+      height: "4.5em",
+    },
   },
   tab: {
     ...theme.themeStyle.typography.tab,
     minWidth: 10,
     marginLeft: "25px",
-    color: 'black',
-    opacity: 0.7
+    color: "black",
+    opacity: 0.7,
   },
   button: {
     borderRadius: "50px",
@@ -68,50 +68,50 @@ const styles = theme => ({
     marginRight: "25px",
     fontSize: "1rem",
     textTransform: "none",
-    height: "45px"
+    height: "45px",
   },
   appbar: {
-    background: '#fefefe',
-    zIndex: theme.zIndex.modal + 1
+    background: "#fefefe",
+    zIndex: theme.zIndex.modal + 1,
   },
   menu: {
     backgroundColor: "#fefefe",
-    borderRadius: "0"
+    borderRadius: "0",
   },
   menuItem: {
     ...theme.themeStyle.typography.tab,
     opacity: 0.7,
     "&:hover": {
-      opacity: 1
-    }
+      opacity: 1,
+    },
   },
   drawerIcon: {
     height: "50px",
-    width: "50px"
+    width: "50px",
   },
   drawerIconContainer: {
     marginLeft: "auto",
     "&:hover": {
-      backgroundColor: "transparent"
-    }
+      backgroundColor: "transparent",
+    },
   },
   drawer: {
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: theme.palette.primary.main,
   },
   drawerItem: {
     ...theme.themeStyle.typography.tab,
     color: "white",
-    opacity: 0.7
+    opacity: 0.7,
   },
   drawerItemSession: {
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
   },
   drawerItemSelected: {
     "& .MuiListItemText-root": {
-      opacity: 1
-    }
-  }
-})
+      opacity: 1,
+    },
+  },
+});
 
 const Navbar = (props) => {
   const { classes, authenticated } = props;
@@ -119,149 +119,235 @@ const Navbar = (props) => {
   const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
   const matches = useMediaQuery(theme.breakpoints.down("md"));
 
-  const [openDrawer, setOpenDrawer] = useState(false)
+  const [openDrawer, setOpenDrawer] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const [openMenu, setOpenMenu] = useState(false);
 
   const handleChange = (e, newValue) => {
-    props.setValue(newValue)
+    props.setValue(newValue);
   };
 
   const handleClick = (e) => {
-    setAnchorEl(e.currentTarget)
-    setOpenMenu(true)
-  }
+    setAnchorEl(e.currentTarget);
+    setOpenMenu(true);
+  };
 
   const handleMenuItemClick = (e, i) => {
     setAnchorEl(null);
     setOpenMenu(false);
-    props.setSelectedIndex(i)
-  }
+    props.setSelectedIndex(i);
+  };
 
   const handleClose = (e) => {
-    setAnchorEl(null)
-    setOpenMenu(false)
-  }
+    setAnchorEl(null);
+    setOpenMenu(false);
+  };
 
   const menuOptions = [
-    {name: "Tutoring", link: "/tutoring", activeIndex: 1, selectedIndex: 0},
-    {name: "Grade 6 to Grade 12", link: "/gradeschool", activeIndex: 1, selectedIndex: 1},
-    {name: "Computer Programming", link: "/computerprogramming", activeIndex: 1, selectedIndex: 2},
-    {name: "Digita Skills", link: "/digitalskills", activeIndex: 1, selectedIndex: 3}
-  ]
+    { name: "Tutoring", link: "/tutoring", activeIndex: 1, selectedIndex: 0 },
+    {
+      name: "Grade 6 to Grade 12",
+      link: "/gradeschool",
+      activeIndex: 1,
+      selectedIndex: 1,
+    },
+    {
+      name: "Computer Programming",
+      link: "/computerprogramming",
+      activeIndex: 1,
+      selectedIndex: 2,
+    },
+    {
+      name: "Digita Skills",
+      link: "/digitalskills",
+      activeIndex: 1,
+      selectedIndex: 3,
+    },
+  ];
 
-  const routes =
-    [
-      {name: "Home", link: "/", activeIndex: 0},
-      {name: "Tutoring", link: "/tutoring", activeIndex: 1, mouseOver: event => handleClick(event), ariaOwns: anchorEl ? "simple-menu" : undefined, ariaHasPopup: anchorEl ? true : undefined}, 
-      {name: "Puzzle World", link: "/puzzleworld", activeIndex: 2}, 
-      {name: "Innovation in Education", link: "/innovationineducation", activeIndex: 3}, 
-      {name: "Login", link: "/login", activeIndex: 4}, 
-      {name: "Signup", link: "/signup", activeIndex: 5}
-    ]
+  const routes = [
+    { name: "Home", link: "/", activeIndex: 0 },
+    {
+      name: "Tutoring",
+      link: "/tutoring",
+      activeIndex: 1,
+      mouseOver: (event) => handleClick(event),
+      ariaOwns: anchorEl ? "simple-menu" : undefined,
+      ariaHasPopup: anchorEl ? true : undefined,
+    },
+    { name: "Puzzle World", link: "/puzzleworld", activeIndex: 2 },
+    {
+      name: "Innovation in Education",
+      link: "/innovationineducation",
+      activeIndex: 3,
+    },
+    { name: "Login", link: "/login", activeIndex: 4 },
+    { name: "Signup", link: "/signup", activeIndex: 5 },
+  ];
 
   useEffect(() => {
-    [...menuOptions, ...routes].forEach(route => {
+    [...menuOptions, ...routes].forEach((route) => {
       switch (window.location.pathname) {
         case `${route.link}`:
           if (props.value !== route.activeIndex) {
-            props.setValue(route.activeIndex)
-            if (route.selectedIndex && route.selectedIndex !== props.selectedIndex) {
-              props.setSelectedIndex(route.selectedIndex)
+            props.setValue(route.activeIndex);
+            if (
+              route.selectedIndex &&
+              route.selectedIndex !== props.selectedIndex
+            ) {
+              props.setSelectedIndex(route.selectedIndex);
             }
           }
           break;
         default:
           break;
       }
-    })
-  }, [props.value, menuOptions, props.selectedIndex, routes, props])
-    
+    });
+  }, [props.value, menuOptions, props.selectedIndex, routes, props]);
+
   const tabs = (
     <Fragment>
-      <Tabs value={props.value} onChange={handleChange} className="tabContainer" indicatorColor="primary">
+      <Tabs
+        value={props.value}
+        onChange={handleChange}
+        className="tabContainer"
+        indicatorColor="primary"
+      >
         {routes.map((route, i) => (
-          <Tab key={`${route}${i}`} className={classes.tab} component={Link} to={route.link} label={route.name} onMouseOver={route.mouseOver} aria-owns={route.ariaOwns} aria-haspopup={route.ariaHasPopup}/>
+          <Tab
+            key={`${route}${i}`}
+            className={classes.tab}
+            component={Link}
+            to={route.link}
+            label={route.name}
+            onMouseOver={route.mouseOver}
+            aria-owns={route.ariaOwns}
+            aria-haspopup={route.ariaHasPopup}
+          />
         ))}
       </Tabs>
-      <Button variant="contained" color="secondary" className={classes.button} component={Link} to="/bookasession">
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        component={Link}
+        to="/bookasession"
+      >
         Book A Tutoring Session
       </Button>
-      <Menu 
-        id="simple-menu" 
-        anchorEl={anchorEl} 
-        open={openMenu} 
-        onClose={handleClose} 
-        MenuListProps={{onMouseLeave: handleClose}}
-        classes={{paper: classes.menu}}
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={handleClose}
+        MenuListProps={{ onMouseLeave: handleClose }}
+        classes={{ paper: classes.menu }}
         elevation={0}
-        style={{zIndex: 1302}}
+        style={{ zIndex: 1302 }}
         keepMounted
       >
         {menuOptions.map((option, i) => (
           <MenuItem
             key={`${option}${i}`}
-            component={Link} 
+            component={Link}
             to={option.link}
-            classes={{root: classes.menuItem}}
-            onClick={(event) => {handleMenuItemClick(event, i); props.setValue(1); handleClose()}}
+            classes={{ root: classes.menuItem }}
+            onClick={(event) => {
+              handleMenuItemClick(event, i);
+              props.setValue(1);
+              handleClose();
+            }}
             selected={i === props.selectedIndex && props.value === 1}
           >
             {option.name}
           </MenuItem>
-        ))}  
+        ))}
       </Menu>
     </Fragment>
-  )
+  );
 
   const drawer = (
     <Fragment>
-      <SwipeableDrawer 
-        disableBackdropTransition={!iOS} 
-        disableDiscovery={iOS} open={openDrawer} 
-        onClose={() => setOpenDrawer(false)} 
-        onOpen={()=>setOpenDrawer(true)}
-        classes={{paper: classes.drawer}}
+      <SwipeableDrawer
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        onOpen={() => setOpenDrawer(true)}
+        classes={{ paper: classes.drawer }}
       >
-      <div className={classes.toolbarMargin}></div>
+        <div className={classes.toolbarMargin}></div>
         <List>
-          {routes.map(route => (
-            <ListItem classes={{selected: classes.drawerItemSelected}} key={`${route}${route.activeIndex}`} divider button component={Link} to={route.link} selected={props.value === route.activeIndex} onClick={() => {setOpenDrawer(false); props.setValue(route.activeIndex)}}>
-              <ListItemText className={classes.drawerItem} disableTypography>{route.name}</ListItemText>
+          {routes.map((route) => (
+            <ListItem
+              classes={{ selected: classes.drawerItemSelected }}
+              key={`${route}${route.activeIndex}`}
+              divider
+              button
+              component={Link}
+              to={route.link}
+              selected={props.value === route.activeIndex}
+              onClick={() => {
+                setOpenDrawer(false);
+                props.setValue(route.activeIndex);
+              }}
+            >
+              <ListItemText className={classes.drawerItem} disableTypography>
+                {route.name}
+              </ListItemText>
             </ListItem>
           ))}
-          <ListItem classes={{root: classes.drawerItemSession, selected: classes.drawerItemSelected }} onClick={() => {setOpenDrawer(false); props.setValue(6)}} divider button component={Link} to="/bookasession" selected={props.value === 6}>
-          <ListItemText 
-              className={classes.drawerItem} 
-              disableTypography
-            >
+          <ListItem
+            classes={{
+              root: classes.drawerItemSession,
+              selected: classes.drawerItemSelected,
+            }}
+            onClick={() => {
+              setOpenDrawer(false);
+              props.setValue(6);
+            }}
+            divider
+            button
+            component={Link}
+            to="/bookasession"
+            selected={props.value === 6}
+          >
+            <ListItemText className={classes.drawerItem} disableTypography>
               Book a Tutoring Session
             </ListItemText>
           </ListItem>
         </List>
       </SwipeableDrawer>
-      <IconButton className={classes.drawerIconContainer} onClick={() => setOpenDrawer(!openDrawer)} disableRipple>
-        <MenuIcon className={classes.drawerIcon}/>
+      <IconButton
+        className={classes.drawerIconContainer}
+        onClick={() => setOpenDrawer(!openDrawer)}
+        disableRipple
+      >
+        <MenuIcon className={classes.drawerIcon} />
       </IconButton>
     </Fragment>
-  )
+  );
 
   return (
     <Fragment>
       <AppBar position="fixed" className={classes.appbar}>
         <Toolbar disableGutters className="nav-container">
-          <Button 
-            component={Link} 
-            to="/" 
-            onClick={() => props.setValue(0)} 
-            className={classes.logoContainer} 
+          <Button
+            component={Link}
+            to="/"
+            onClick={() => props.setValue(0)}
+            className={classes.logoContainer}
             disableRipple
           >
-            <img alt="Company Logo" src={upLogo} className={classes.headerLogo} />
+            <img
+              alt="Company Logo"
+              src={upLogo}
+              className={classes.headerLogo}
+            />
           </Button>
           {authenticated ? (
             <Fragment>
-              <PostPuzzlepiece/>
+              <PostPuzzlepiece />
               <Link to="/">
                 <MyButton tip="Home">
                   <HomeIcon />
@@ -270,25 +356,22 @@ const Navbar = (props) => {
               <Notifications />
             </Fragment>
           ) : (
-            <Fragment>
-              {matches ? drawer : tabs}
-            </Fragment>
+            <Fragment>{matches ? drawer : tabs}</Fragment>
           )}
         </Toolbar>
       </AppBar>
       <div className={classes.toolbarMargin}></div>
     </Fragment>
-  )
-}
-
+  );
+};
 
 Navbar.propTypes = {
   authenticated: PropTypes.bool.isRequired,
-  classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = (state) => ({
-  authenticated: state.user.authenticated
-})
+  authenticated: state.user.authenticated,
+});
 
 export default connect(mapStateToProps)(withStyles(styles)(Navbar));
--- unpuzzle-client/src/pages/home.js
+++ unpuzzle-client/src/pages/home.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import Lottie from 'react-lottie';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Lottie from "react-lottie";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import HeaderAnimation from "../images/svg-assets/unpuzzle-header";
+import ButtonArrow from "../components/layout/ButtonArrow";
 
-import animationData from '../images/svg-assets/unpuzzle-header';
-
-const useStyles = makeStyles(theme => ({
-
-}))
+const useStyles = makeStyles((theme) => ({}));
 
 export default function Home() {
   const classes = useStyles();
 
   const defaultOptions = {
     loop: false,
-    autoplay: true, 
-    animationData: animationData,
+    autoplay: true,
+    animationData: HeaderAnimation,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  }
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-  return <Lottie options={defaultOptions} height={"100%"} width={"75%"} />
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container justify="flex-end" direction="row">
+          <Grid item>
+            <div>
+              The most innovative online education platform in the world.
+            </div>
+            <Grid container>
+              <Grid item>
+                <Button variant="contained">Book A Tutoring Session</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">
+                  Learn More
+                  <ButtonArrow width={15} height={15} fill="red" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.lottie}>
+            <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
